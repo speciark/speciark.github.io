8f03895b980b196c9c4be1fd3d467efb1068ddbe
PHENOL0000-0000 <PHENOL0000.0000@gmail.com>
2024-08-21 19:58:32 +0900

    make urldb.js

---
+++ urldb.js
@@ -0,0 +1,48 @@
+const reponame = "urldb";
+var scriptUrl = document.currentScript.src;
+const urn = scriptUrl.substring(0, scriptUrl.lastIndexOf('/'));
+const path = reponame;
+
+const seps = "\n\t<p> [URL DB : 뭉탱이로 왔다가 유링게슝] </p><br>\n\t"
+var firstihint = '<div id="jsondb">'.length;
+var lastihint = "</div><br>".length;
+const firsti = firstihint--;
+const lasti = -lastihint;
+const url = "https://" + urn + '/' + path + '?';
+
+function Loads(txt){
+	var text = txt;
+	var text = text.split(seps)
+	text.pop();
+	text.shift();
+	var text = text.join(seps);
+	var text = text.slice(firsti, lasti);
+	return JSON.parse(text); 
+}
+
+async function urldbload(url) {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error('ConnectionFailed');
+		}
+		const data = await response.text();
+		var ret = Loads(data);
+		return ret;
+	} catch (error) {
+		console.error('Fetch Error:', error);
+		return null;
+	}
+}
+
+function urldbdump(jsondb){
+	var keys = Object.keys(jsondb);
+	var ret = [];
+	for(var key of keys)  {
+  		var v = jsondb[key];
+  		var arrs = [key, v];
+  		var line = arrs.join("=");
+  		ret.push(line);
+ 	}
+	return url + ret.join("&");
+}
